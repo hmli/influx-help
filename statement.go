00149a20b947f940671062fdfcf405b335cd8f3e
lihm <lihm>
2018-02-24 17:01:13 +0800

    可生成基本的select语句，还不支持groupby, order等

--- statement.go
+++ statement.go
@@ -3,8 +3,14 @@ package influx_help
 import (
 	"strings"
 	"github.com/go-xorm/builder"
+	"fmt"
+	"bytes"
+	"github.com/influxdata/influxdb/client/v2"
+	"time"
+	"errors"
+	"strconv"
 )
-
+var ErrSkip = errors.New("skip fast-path; continue as if unimplemented")
 type Statement struct {
 	Start           int
 	LimitN          int
@@ -25,9 +31,10 @@ type Statement struct {
 	//IsDistinct      bool
 	TableAlias      string
 	cond            builder.Cond
+	Database string
 }
 
-func (stmt *Statement) Init() {
+func (stmt *Statement) Init(database string) {
 	stmt.Start = 0
 	stmt.LimitN = 0
 	stmt.OrderStr = ""
@@ -38,6 +45,7 @@ func (stmt *Statement) Init() {
 	stmt.RawSQL = ""
 	stmt.RawParams = make([]interface{}, 0)
 	stmt.cond = builder.NewCond()
+	stmt.Database = database
 }
 
 func (stmt *Statement) SQL(query string, args ...interface{}) *Statement {
@@ -105,12 +113,113 @@ func (stmt *Statement) OrderBy(order string) *Statement {
 	return stmt
 }
 
-func (stmt *Statement) Query() (sql string) {
+func (stmt *Statement) Query() (query *client.Query, err error) {
+	condSQL, condArgs, err := stmt.condSQL()
+	if err != nil {
+		return nil, err
+	}
+	columnSQL := stmt.columnSQL()
+	queryStr := stmt.selectSQLNoArgs(columnSQL, condSQL)
+	selectSQL, err := stmt.selectSQL(queryStr, condArgs)
+	if err != nil {
+		return nil, err
+	}
+	q := client.Query{Command: selectSQL, Database: stmt.Database}
+	return &q, nil
+}
+
+func (stmt *Statement) condSQL() (sql string, args []interface{}, err error) {
+	// TODO 先使用 xorm 自带的 builder，后面还需要修改，要补上一些 ' 等。
+	sql, args, err = builder.ToSQL(stmt.cond)
 	return
 }
 
-// TODO test Cols/Select, Cond, Limit
-// TODO 1. Desc,Asc  2. GroupBy  3. Make SQL
+func (stmt *Statement) columnSQL() string{
+	if stmt.selectStr != "" {
+		return stmt.selectStr
+	}
+	return stmt.ColumnStr
+}
+
+// selectSQL 拼接出select SQL语句，但参数仍以？代替
+func (stmt *Statement) selectSQLNoArgs(columnStr, condSQL string) string {
+	var buf bytes.Buffer
+	if len(condSQL) > 0 {
+		fmt.Fprintf(&buf, " WHERE %v", condSQL)
+	}
+	var whereStr = buf.String()
+	var fromStr = " FROM "
+	time.Now().Add(-24 * time.Hour)
+	return fmt.Sprintf("SELECT %v%v%v", columnStr, fromStr, whereStr)
+}
+
+func (stmt *Statement) selectSQL(query string, args []interface{}) (a string, err error) {
+	// TODO 目前是用MySQL的格式拼接的，需测试改到influx格式
+	if strings.Count(query, "?") != len(args) {
+		return "", ErrSkip
+	}
+	argPos := 0
+	buf := make([]byte, 0) // TODO buf pool
+	for i:=0; i<len(query); i++ {
+		q := strings.IndexByte(query[i:], '?')
+		if q == -1 {
+			buf = append(buf, query[i:]...)
+			break
+		}
+		buf = append(buf, query[i:i+q]...)
+		i += q
+
+		arg := args[argPos]
+		argPos++
+
+		if arg == nil {
+			buf = append(buf, "NULL"...)
+			continue
+		}
+		switch v := arg.(type) {
+		case int64:
+			buf = strconv.AppendInt(buf, v, 10)
+		case int:
+			buf = strconv.AppendInt(buf, int64(v), 10)
+		case int32:
+			buf = strconv.AppendInt(buf, int64(v), 10)
+		case int16:
+			buf = strconv.AppendInt(buf, int64(v), 10)
+		case int8:
+			buf = strconv.AppendInt(buf, int64(v), 10)
+		case float64:
+			buf = strconv.AppendFloat(buf, v, 'g', -1, 64)
+		case float32:
+			buf = strconv.AppendFloat(buf, float64(v), 'g', -1, 64)
+		case bool:
+			if v {
+				buf = append(buf, '1')
+			} else {
+				buf = append(buf, '0')
+			}
+		case time.Time:
+			if v.IsZero() {
+				buf = append(buf, "'0000-00-00'"...)
+			} else {
+				buf = append(buf, []byte(v.Format("'2006-01-02 15:04:05'"))...)
+			}
+		case []byte:
+			buf = append(buf, '\'')
+			buf = append(buf, v...)
+			buf = append(buf, '\'')
+		case string:
+			buf = append(buf, '\'')
+			buf = append(buf, []byte(v)...)
+			buf = append(buf, '\'')
+		default:
+			return "", ErrSkip
+		}
+	}
+	return string(buf), nil
+
+}
+
+// TODO 1. Desc,Asc  2. GroupBy  
 
 var QuoteStr = "'"
 
--- statement_test.go
+++ statement_test.go
@@ -4,8 +4,15 @@ import "testing"
 
 func TestSelectStr(t *testing.T) {
 	stmt := Statement{}
-	stmt.Init()
+	stmt.Init("testlog")
 	stmt.Select("id, content")
 	stmt.Table("testlog")
-	t.Log(stmt.selectStr)
+	stmt.And("id = ?", 5).And("price = ?", 3.4).Or("name = 'ddd'")
+	condSQL, condArgs, err := stmt.condSQL()
+	t.Log(err)
+	t.Log(condArgs)
+	t.Log(stmt.columnSQL())
+	query := stmt.selectSQLNoArgs(stmt.columnSQL(), condSQL)
+	t.Log(query)
+	t.Log(stmt.selectSQL(query, condArgs))
 }
